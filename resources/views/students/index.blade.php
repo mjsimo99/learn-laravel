256888b2022b31ab7cf60e3e33fc4b4c5bf85a26
mjsimo99 <mohashark21@gmail.com>
2023-02-10 17:14:18 +0100

    new ajout

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,14 @@
+@extends('students.layout')
+@section('content')
+<div class="container">
+    <div class="row" style="margin: 20px;">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Laravel 9 Crud (Create, Read, Update and Delete</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
